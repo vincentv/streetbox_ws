259ab700b2e5d36caabc1c48b250b1281e719ccb
Vincentv <lsuntzu@gmail.com>
2012-06-24 00:55:46 +0200

    ajout d'un parametre ordered

--- routes/spots.js
+++ routes/spots.js
@@ -10,24 +10,43 @@ exports.spots = {}
  * Retourne la liste des playlist
 */
 exports.spots.get = function(req, res){
-    var result = [], query, lat, lon, radius;
+  var result = [], query, lat, lon, radius;
 
-    limit = parseFloat(req.param('limit'));
-    maxDistance = parseFloat(req.param('maxDistance'));
-    lat = parseFloat(req.param('lat'));
-    lon = parseFloat(req.param('lon'));
+  limit = parseFloat(req.param('limit'));
+  maxDistance = parseFloat(req.param('maxDistance'));
+  lat = parseFloat(req.param('lat'));
+  lon = parseFloat(req.param('lon'));
+  ordered = (_.isUndefined(req.param('ordered'))) ? 1 : parseInt(req.param('ordered'));
 
+  if (1 === ordered) {
     query = mongoose.model('Spot')
     .find(
-        {
-            "loc": {
-                "$near": [lon, lat],
-                "$maxDistance": maxDistance
-            }
-        }
-    ).limit(limit);
+      {
+      "loc": {
+        "$near": [lon, lat],
+        "$maxDistance": maxDistance
+      }
+    }
+    );
 
-    query.exec(function(err, elmts){
-        res.send(elmts);
-    });
+  } else {
+    query = mongoose.model('Spot')
+    .find(
+      {
+      "loc": {
+        "$within": {
+          "$center": [
+            [lon, lat]
+            , maxDistance
+          ]
+        }
+      }
+    }
+    );
+  }
+
+  query.limit(limit)
+  query.exec(function(err, elmts){
+    res.send(elmts);
+  });
 }
